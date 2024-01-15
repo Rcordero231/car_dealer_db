d1a650bbc3999b14432346828c1069a5b4cba447
Ricardo <rcordero231@gmail.com>
2024-01-15 15:42:31 -0800

    initial commit

---
+++ car_dealership.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Salesperson (
+    salesperson_id INT PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    salesperson_phone VARCHAR NOT NULL,
+    salesperson_email VARCHAR NOT NULL
+);
